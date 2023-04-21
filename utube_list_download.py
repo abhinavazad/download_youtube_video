40d2dc5d616859c6509f9415bb21801dca4a1864
Abhinav <44549945+abhinavazad@users.noreply.github.com>
2023-04-21 21:18:42 +0200

    Update utube_list_download.py

--- utube_list_download.py
+++ utube_list_download.py
@@ -10,7 +10,7 @@ This script can be used to download a series of videos from a given list of link
 In this script we extract the list links form a column of an excel file.
 
 Parameters: 
-        1. filenmae        : Path of the video_list.xlx file from where the links will be extracted.
+        1. filename        : Path of the video_list.xlx file from where the links will be extracted.
         2. columnname      : Name of the column which consists the video links in the video_list.xlx file 
 
 Output:
